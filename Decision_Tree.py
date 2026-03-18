d56363913548424033f6911a0f292f9e49c8f56e
Mariana-PerezPerera <mariana.perezperera@ucalgary.ca>
2026-03-18 17:39:57 -0600

    importing packages.

--- Decision_Tree.py
+++ Decision_Tree.py
@@ -1,2 +1,5 @@
-#test
-#testing
+#importing the needed packages:
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
